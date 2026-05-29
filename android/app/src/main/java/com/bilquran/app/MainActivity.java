ed16fbc9a70c59196a7b7272e280459c60858c33
Alimnfl <mnaufallim234@gmail.com>
2026-05-29 18:05:06 +0700

    Add capacitor `android` and `ios`.

---
+++ android/app/src/main/java/com/bilquran/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.bilquran.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,7 @@
+const config = {
+  appId: "com.bilquran.app",
+  appName: "Bil Quran",
+  webDir: "build",
+}
+
+export default config
