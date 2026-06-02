ccb0fa3997343573c557459b051b3347362fee42
Alimnfl <mnaufallim234@gmail.com>
2026-06-02 09:02:32 +0700

    Update the capacitor config.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,13 @@
-const config = {
+import { CapacitorConfig } from "@capacitor/cli"
+
+const config: CapacitorConfig = {
   appId: "com.bilquran.app",
-  appName: "Bil Quran",
+  appName: "BilQuran",
   webDir: "build",
+  server: {
+    url: "http://192.168.1.4:3000",
+    cleartext: true,
+  },
 }
 
 export default config
